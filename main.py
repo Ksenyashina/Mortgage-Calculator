76c6febcf78eb257fd65fbc3adcfff5e679104f3
Ksenyashina <Ksenya421@mail.ru>
2024-03-08 23:20:34 +0300

    Auto build APK file 64bit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from kivy.lang import Builder
 from kivy.metrics import dp
-
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.tab import MDTabsBase
 from kivymd.app import MDApp
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.snackbar import Snackbar
+from kivymd.icon_definitions import md_icons
 
 KV = '''
 MDBoxLayout:
@@ -11,8 +13,8 @@ MDBoxLayout:
 
     MDTopAppBar:
         title: "MortgageCalculator"
-        left_action_items: [["menu", lambda x: app.callback(x)]]
-        
+    MDTabs:
+        id: tabs
 
     MDLabel:
         text: "Content"
@@ -20,45 +22,28 @@ MDBoxLayout:
     Widget:
         MDTextField:
             hint_text:"No helper text"
+
 '''
 
-
+class Tab(MDFloatLayout, MDTabsBase):
+    pass
 class Test(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Purple"
-        self.theme_cls.theme_style = "Dark"
-        menu_items = [
-            {
-                "text": f"Ввод",
-                "on_release": lambda x=f"Ввод": self.menu_callback(x),
-            },
-            {
-                "text": f"Расчет",
-                "on_release": lambda x=f"Расчет": self.menu_callback(x),
-            },
-            {
-                "text": f"График",
-                "on_release": lambda x=f"График": self.menu_callback(x),
-            },
-            {
-                "text": f"Диаграмма",
-                "on_release": lambda x=f"Диаграмма": self.menu_callback(x),
-            },
-            {
-                "text": f"Итого",
-                "on_release": lambda x=f"Итого": self.menu_callback(x),
-            }
-        ]
-        self.menu = MDDropdownMenu(items=menu_items)
+        self.theme_cls.theme_style = "Light"
         return Builder.load_string(KV)
-
-    def callback(self, button):
-        self.menu.caller = button
-        self.menu.open()
-
-    def menu_callback(self, text_item):
-        self.menu.dismiss()
-        Snackbar(text=text_item).open()
+    def on_start(self):
+        icons_item = {
+                "Ввод": "Ввод",
+                "Расчет": "Расчет",
+                "График": "График",
+                "Диаграмма": "Диаграмма",
+                "Итого": "Итого"
+            }
+        # for name_tab in list(md_icons.keys())[15:30]:
+        #     self.root.ids.tabs.add_widget(Tab(icon=name_tab, title=name_tab))
+        for icon_name, name_tab in icons_item.items():
+            self.root.ids.tabs.add_widget(Tab(title=name_tab))
 
 
 Test().run()
