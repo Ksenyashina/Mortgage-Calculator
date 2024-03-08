91ebd82a7e26baadffb8d523a85e7ed0c653fff6
Ksenyashina <Ksenya421@mail.ru>
2024-03-08 14:10:17 +0300

    Auto build APK file 64bit

--- main.py
+++ main.py
@@ -1,10 +1,64 @@
+from kivy.lang import Builder
+from kivy.metrics import dp
+
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.snackbar import Snackbar
+
+KV = '''
+MDBoxLayout:
+    orientation: "vertical"
+
+    MDTopAppBar:
+        title: "MortgageCalculator"
+        left_action_items: [["menu", lambda x: app.callback(x)]]
+        
+
+    MDLabel:
+        text: "Content"
+        halign: "center"
+    Widget:
+        MDTextField:
+            hint_text:"No helper text"
+'''
 
 
-class MortgageCalculatorApp(MDApp):
+class Test(MDApp):
     def build(self):
-        return MDLabel(text="Hello, Mortgage Calculator", halign="center")
+        self.theme_cls.primary_palette = "Purple"
+        self.theme_cls.theme_style = "Dark"
+        menu_items = [
+            {
+                "text": f"Ввод",
+                "on_release": lambda x=f"Ввод": self.menu_callback(x),
+            },
+            {
+                "text": f"Расчет",
+                "on_release": lambda x=f"Расчет": self.menu_callback(x),
+            },
+            {
+                "text": f"График",
+                "on_release": lambda x=f"График": self.menu_callback(x),
+            },
+            {
+                "text": f"Диаграмма",
+                "on_release": lambda x=f"Диаграмма": self.menu_callback(x),
+            },
+            {
+                "text": f"Итого",
+                "on_release": lambda x=f"Итого": self.menu_callback(x),
+            }
+        ]
+        self.menu = MDDropdownMenu(items=menu_items)
+        return Builder.load_string(KV)
+
+    def callback(self, button):
+        self.menu.caller = button
+        self.menu.open()
+
+    def menu_callback(self, text_item):
+        self.menu.dismiss()
+        Snackbar(text=text_item).open()
 
 
-MortgageCalculatorApp().run()
+Test().run()
